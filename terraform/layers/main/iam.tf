d65868aeca6186d1f248793433faf011ff3209bb
ThibaultLengagne <thibaultl@padok.fr>
2024-04-18 18:20:50 +0200

    test permission
    Signed-off-by: ThibaultLengagne <thibaultl@padok.fr>

--- terraform/layers/main/iam.tf
+++ terraform/layers/main/iam.tf
@@ -1,20 +1,4 @@
-resource "aws_iam_policy" "admin-access" {
-  name        = "demo-policy"
-  description = "S3 read policy"
-
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "ListObjectsInBucket",
-            "Effect": "Allow",
-            "Action": ["s3:*"],
-            "Resource": ["*"]
-        }
-    ]
-  })
-}
-
+# OIDC Trust
 resource "aws_iam_openid_connect_provider" "default" {
   url = "https://token.actions.githubusercontent.com"
 
@@ -25,6 +9,7 @@ resource "aws_iam_openid_connect_provider" "default" {
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
+# IAM Role and trust policy
 resource "aws_iam_role" "github" {
   name               = "github-oidc"
   path               = "/"
@@ -50,6 +35,25 @@ resource "aws_iam_role" "github" {
   )
 }
 
+# IAM Policy
+resource "aws_iam_policy" "github-access" {
+  name        = "demo-policy"
+  description = "S3 read policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "ListObjectsInBucket",
+            "Effect": "Allow",
+            "Action": ["s3:*"],
+            "Resource": ["*"]
+        }
+    ]
+  })
+}
+
+# Attach the policy to the role
 resource "aws_iam_policy_attachment" "admin-policy" {
   name       = "admin-attachment"
   roles      = [aws_iam_role.github.name]
